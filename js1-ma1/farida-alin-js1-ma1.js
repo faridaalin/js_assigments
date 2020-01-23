2543d0f83c70be7e026f0ccfcfa081b53c398436
faridaalin <56302698+faridaalin@users.noreply.github.com>
2020-01-23 22:00:48 +0100

    update

---
+++ js1-ma1/farida-alin-js1-ma1.js
@@ -0,0 +1,44 @@
+// question 1
+
+const cat = {
+    complain: function() {
+        console.log("Meow!");
+    }
+};
+
+// question 2
+
+// your answer for question 1 goes here
+
+const headin = document.querySelector("h3");
+
+// your answer for question 1 goes here
+
+// question 4
+
+// your answer for question 1 goes here
+
+// question 5
+
+// your answer for question 1 goes here
+
+// question 6
+
+// your answer for question 1 goes here
+
+// question 7
+
+// your answer for question 1 goes here
+
+// question 8
+
+// your answer for question 1 goes here
+
+// question 9
+
+// your answer for question 1 goes here
+
+// question 10
+
+// your answer for question 1 goes here
+
