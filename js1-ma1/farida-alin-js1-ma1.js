a62130e345a8bf44116571cb4d983ed8dbdd6371
faridaalin <56302698+faridaalin@users.noreply.github.com>
2020-01-24 12:20:05 +0100

    update 2

--- js1-ma1/farida-alin-js1-ma1.js
+++ js1-ma1/farida-alin-js1-ma1.js
@@ -8,37 +8,112 @@ const cat = {
 
 // question 2
 
-// your answer for question 1 goes here
+const heading = document.querySelector("h3");
 
-const headin = document.querySelector("h3");
+// question 3
 
-// your answer for question 1 goes here
+heading.style.fontSize = "2em";
 
 // question 4
 
-// your answer for question 1 goes here
+heading.classList.add("subheading");
 
 // question 5
 
-// your answer for question 1 goes here
+const paragraphs = document.querySelectorAll("p");
 
 // question 6
 
-// your answer for question 1 goes here
+const resultsContainer = document.querySelector(".results");
+resultsContainer.innerHTML = "<p>New paragraph</p>";
 
 // question 7
 
-// your answer for question 1 goes here
+const cats = [
+    {
+        name: "Blob",
+        age: 10,
+    },
+    {
+        name: "Harold",
+    },
+    {
+        name: "Blurt",
+        age: 21
+    }
+];
+
+function catFunction (catArray) {
+    for (let i = 0; i < catArray.length; i++) {
+        console.log(catArray[i].name);
+    }
+};
+catFunction(cats);
 
 // question 8
 
-// your answer for question 1 goes here
+function catFunction (catArray) {
+    let name = "";
+    for (let i = 0; i < catArray.length; i++) {
+        name = `<h5>${catArray[i].name}</h5>`;
+        console.log(name);
+        
+    }
+    //  return name;
+};
+catFunction(cats);
+
 
 // question 9
 
-// your answer for question 1 goes here
+// catFunction(cats);
+// resultsContainer.innerHTML = name;
+
+
 
 // question 10
 
-// your answer for question 1 goes here
+
+
+// function catFunction (catArray) {
+//     let name = "";
+//     let ageMissing = "Age Uknown"; 
+
+//     for (let i = 0; i < catArray.length; i++) {
+         
+//     if(catArray[i].age) {
+//
+//         catArray[i].age;
+//     } else {
+//         let ageMissing = "Age Uknown"; 
+//     }
+
+//         name = `<div>
+//                 <h5>${catArray[i].name}</h5>
+//                 <p>${catArray[i].age}</p>
+//                 </div>`;
+
+//         console.log(name);
+        
+//     }
+    
+// };
+
+// catFunction(cats);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
