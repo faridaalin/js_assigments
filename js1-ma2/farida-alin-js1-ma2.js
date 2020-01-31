b39f2b6dc00c89d1a9519fd1efd049e80ee94478
faridaalin <56302698+faridaalin@users.noreply.github.com>
2020-01-31 13:21:14 +0100

    MA assigment 2

---
+++ js1-ma2/farida-alin-js1-ma2.js
@@ -0,0 +1,94 @@
+// question 1
+const myFunctionExpression = function() {
+  console.log("Farida");
+};
+
+myFunctionExpression();
+
+// question 2
+const button = document.querySelector(".btn");
+button.addEventListener("click", buttonClicked);
+
+function buttonClicked() {
+    console.log('I was clicked');
+}
+
+// question 3
+const inputField = document.querySelector("#firstName");
+inputField.addEventListener("keydown", function valueOfKeydown() {
+    console.dir(event.target.value);
+  });
+
+function valueOfKeydown() {
+  console.dir(event.target.value);
+}
+
+// question 4
+const buttonHover = document.querySelector("button");
+buttonHover.addEventListener("mouseover", function addHover() {
+    buttonHover.classList.add("hover");
+  });
+
+
+// question 5
+const buttonData = document.querySelector('[data-animal="dog"]');
+
+buttonData.addEventListener("mouseout", function() {
+  buttonData.classList.remove("hover");
+});
+
+//question 6
+const list = document.querySelectorAll("li");
+
+for (let i = 0; i < list.length; i++) {
+  list[i].addEventListener("mouseover", printOutDataValue);
+}
+
+function printOutDataValue(event) {
+  const listDataValue = event.target.dataset.animal;
+  console.log(listDataValue);
+}
+
+//question 7
+const animal = "cow";
+
+switch (animal) {
+  case "cat":
+    console.log("Meow");
+    break;
+  case "cow":
+    console.log("Moo");
+    break;
+  case "bird":
+    console.log("Tweet");
+    break;
+  default:
+    console.log("Harrumph");
+}
+
+// question 8
+const sheep = ["Malcom", "Anders", "Marie"];
+
+sheep.forEach(function(sheeps) {
+  console.log(sheeps);
+});
+
+// question 9
+let counter = 0;
+
+const interValId = setInterval(function logTime() {
+    console.log("Hello");
+    if (counter === 5) {
+      clearInterval(interValId);
+    }
+    counter++;
+  }, 500);
+
+// question 10
+const divContainer = document.querySelector(".container");
+
+let newContent = "Text updated";
+
+setTimeout(function() {
+  divContainer.innerHTML = newContent;
+}, 2000);
